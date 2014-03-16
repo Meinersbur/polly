aebb20e4828ca9409044c8a74c2004891b2c0f87
Michael Kruse <michael.kruse@inria.fr>
2014-03-16 18:34:47 +0100

    Compile fix

--- tools/pollycc/cc1_main.cpp
+++ tools/pollycc/cc1_main.cpp
@@ -61,7 +61,7 @@ static void LLVMErrorHandler(void *UserData, const std::string &Message,
 
 int cc1_main(const char **ArgBegin, const char **ArgEnd,
   const char *Argv0, void *MainAddr) {
-  OwningPtr<CompilerInstance> Clang(new CompilerInstance());
+  std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
   IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
 
   // Initialize targets first, so that --version shows registered targets.
@@ -115,7 +115,7 @@ int cc1_main(const char **ArgBegin, const char **ArgEnd,
   if (Clang->getFrontendOpts().DisableFree) {
     if (llvm::AreStatisticsEnabled() || Clang->getFrontendOpts().ShowStats)
       llvm::PrintStatistics();
-    BuryPointer(Clang.take());
+    BuryPointer(Clang.release());
     return !Success;
   }
 
