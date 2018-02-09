2ca5e826ae0057f8b70b4ef43f3b169cbfc493ed
Oleksandr Zinenko <git@ozinenko.com>
2018-02-09 17:16:03 +0100

    call consecutivity from ScheduleOptimizer

--- lib/Transform/ScheduleOptimizer.cpp
+++ lib/Transform/ScheduleOptimizer.cpp
@@ -48,6 +48,7 @@
 
 #include "polly/ScheduleOptimizer.h"
 #include "polly/CodeGen/CodeGeneration.h"
+#include "polly/Consecutivity/consecutivity.h"
 #include "polly/DependenceInfo.h"
 #include "polly/LinkAllPasses.h"
 #include "polly/Options.h"
@@ -1594,12 +1595,28 @@ bool IslScheduleOptimizer::runOnScop(Scop &S) {
   auto OnErrorStatus = isl_options_get_on_error(Ctx);
   isl_options_set_on_error(Ctx, ISL_ON_ERROR_CONTINUE);
 
+  // Construct the consecutivity constraints.
+  auto allAccesses = S.getAccesses();
+  isl_space_list *arraySpaces = isl_space_list_alloc(allAccesses.get_ctx().get(),
+      1);
+  allAccesses.foreach_map([&arraySpaces](isl::map m) {
+    arraySpaces = isl_space_list_add(arraySpaces, 
+        m.get_space().range().release());
+    return isl::stat::ok;
+  });
+  auto consecutive = ppcg_consecutive_from_array_list(arraySpaces);
+
   auto SC = isl::schedule_constraints::on_domain(Domain);
   SC = SC.set_proximity(Proximity);
   SC = SC.set_validity(Validity);
   SC = SC.set_coincidence(Validity);
+  SC = isl::manage(ppcg_consecutive_add_consecutivity_constraints(
+      consecutive, SC.release()));
+  ppcg_consecutive_free(consecutive);
+
   auto Schedule = SC.compute_schedule();
   isl_options_set_on_error(Ctx, OnErrorStatus);
+
 
   walkScheduleTreeForStatistics(Schedule, 1);
 
